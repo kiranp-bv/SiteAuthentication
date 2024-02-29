106a8ceaffa13df9a89504f09c0d67ada83c442f
Kiran P <kiran.p@bazaarvoice.com>
2024-02-29 14:51:54 +0530

    aaa

--- video/upload/index.js
+++ video/upload/index.js
@@ -1,7 +1,7 @@
 let mediaRecorder;
 let chunks = []
 const videoEle = document.querySelector("#live")
-const videoSrcEle = document.querySelector("#videoSource")
+const videoSrcEle = document.querySelector("#videoSource-mp4")
 const output = document.querySelector("#output")
 const inputFileEle = document.querySelector("#inputfile")
 const resultDiv = document.querySelector("#permissionresult")
@@ -9,9 +9,42 @@ const errorResultDiv = document.querySelector("#permissionresult")
 const createBtn1 = document.querySelector("#createbtn1")
 const createBtn2 = document.querySelector("#createbtn2")
 const stopBtn = document.querySelector("#stopbtn")
-const videocontainer = document.querySelector("#videocontainer")
+const options = {
+    // mimeType: "video/mp4"
+}
+let currentType = 'mp4'
+// alert("Init")
 
+function getVideoSourceEle(type = 'video/mp4') {
+    let type1 = 'mp4'
+    switch (type) {
+        case 'video/mp4':
+            type1 = 'mp4';
+            break;
+        case 'video/avi':
+            type1 = 'mov';
+            break;
+        case 'video/x-ms-asf':
+            type1 = 'asf'
+            break;
 
+        case 'video/quicktime':
+            type1 = 'mov'
+            break;
+
+            case 'video/x-msvideo':
+                type1 = 'mov'
+                break;
+
+                case 'video/mpeg':
+                    type1 = 'mpeg'
+                    break;
+            
+            	
+    }
+    currentType = type
+    return document.querySelector(`#videoSource-${type1}`)
+}
 function create(e) {
     inputFileEle.click()
 }
@@ -21,13 +54,18 @@ function handleFileInput(e) {
     console.log("Handling file input");
     const uploadedFile = e?.target?.files[0];
     if (uploadedFile) {
+        let srcEle = getVideoSourceEle(currentType)
+        // srcEle.src = null
+        // reset src value for prev video type
+
         videocontainer.style.display = "block"
-        console.log("Type of uploaded file : ", typeof e?.target?.files[0] );
+        console.log("Type of uploaded file : ", uploadedFile, uploadedFile.type );
         const url = URL.createObjectURL(uploadedFile)
+        srcEle = getVideoSourceEle(uploadedFile.type)
         videoEle.srcObject = null;
         videoEle.controls = true;
         videoEle.loop = true;
-        videoSrcEle.src = url;
+        srcEle.src = url;
         videoEle.load()
         // videoEle.play().then(() => {
 
@@ -61,7 +99,38 @@ function handleVideoError(e){
     );
 }
 
+function waiting(e) {
+    console.log("Waiting : ", e);
+}
 
+function loadeddata(e) {
+    console.log("Loaded data : ", e);
+}
+
+function loadStart(e) {
+    console.log("loadStart : ", e);
+}
+
+function suspend(e) {
+    console.log("suspend : ", e);
+}
+
+function stalled(e) {
+    console.log("stalled : ", e);
+}
+
+function handleEvent(event) {
+    console.log(`${event.type}`);
+  }
 createBtn1.addEventListener('click', create)
 inputFileEle.addEventListener('change', handleFileInput)
 videoEle.addEventListener('error', handleVideoError)
+videoEle.addEventListener('waiting', handleEvent)
+videoEle.addEventListener('loadeddata', handleEvent)
+videoEle.addEventListener('suspend', handleEvent)
+videoEle.addEventListener('stalled', handleEvent)
+videoEle.addEventListener("loadstart", handleEvent);
+videoEle.addEventListener("progress", handleEvent);
+videoEle.addEventListener("canplay", handleEvent);
+videoEle.addEventListener("canplaythrough", handleEvent);
+
